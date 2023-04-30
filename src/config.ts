5ea18cb63c2e9d874719db21324cdc673b3da6ab
vaaski <admin@vaa.ski>
2023-04-30 09:21:43 +0200

    feat: save key in conf

--- src/config.ts
+++ src/config.ts
@@ -1,1 +1,19 @@
-import {} from "conf"
+import Conf from "conf"
+
+export type HowtfRequest = {
+  query: string
+  response: string
+}
+
+export type Model = "gpt-3.5-turbo" | "gpt-4"
+
+const defaults = {
+  key: undefined as string | undefined,
+  model: "gpt-3.5-turbo" as Model,
+  lastRequest: undefined as HowtfRequest | undefined,
+}
+
+export const config = new Conf({
+  projectName: "howtf",
+  defaults,
+})
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,26 @@
 #! /usr/bin/env node
 
-console.log("Hello, world!")
+import { config } from "./config"
+import prompts from "prompts"
+
+const main = async () => {
+  let key = config.get("key")
+  if (!key) {
+    const response = await prompts({
+      type: "text",
+      name: "key",
+      message: "Enter your OpenAI API key",
+      validate: Boolean,
+    })
+
+    if (typeof response.key !== "string") throw new Error("No key provided")
+
+    config.set("key", response.key)
+    key = response.key
+  }
+
+  console.log("Your key is", key)
+}
+
+main()
 export {}
