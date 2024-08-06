b8fa1095cc18ecdec60ee3bec2296c533200350a
vaaski <admin@vaa.ski>
2024-08-07 00:18:55 +0200

    quit on query page

--- page-query.go
+++ page-query.go
@@ -28,10 +28,22 @@ func queryView(m *model) string {
 
 func queryController(m *model, msg tea.Msg) (tea.Model, tea.Cmd) {
 	if !m.query.initialized {
-		m.query.initialized = true
-
 		if len(m.args) == 0 {
 			m.args = append(m.args, "should ask for input")
+		}
+
+		switch msg := msg.(type) {
+		case tea.KeyMsg:
+			switch msg.Type {
+			case tea.KeyEnter:
+				m.config.openAIToken = m.config.tokenInput.Value()
+				m.config.tokenInput.Blur()
+
+				return m, nil
+
+			case tea.KeyEsc:
+				return m, tea.Quit
+			}
 		}
 	}
 
