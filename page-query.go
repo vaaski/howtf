90a889ce12650143ff766255a8aee53cf5063a89
vaaski <admin@vaa.ski>
2024-08-07 00:41:04 +0200

    query input

--- page-query.go
+++ page-query.go
@@ -3,30 +3,42 @@ package main
 import (
 	"strings"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 type queryModel struct {
 	initialized bool
 	responseRaw string
+
+	input textinput.Model
 }
 
 func queryInitialModel() queryModel {
-	return queryModel{}
+	query := queryModel{}
+
+	query.input = textinput.New()
+	query.input.Placeholder = "Enter query"
+	query.input.Prompt = ": "
+
+	return query
 }
 
 func queryView(m *model) string {
 	var s string
 
-	s += "Query:"
+	s += "QUERY"
 	s += "\n\n"
 	s += strings.Join(m.args, " ")
+	s += m.query.input.View()
 	s += "\n"
 
 	return s
 }
 
 func queryController(m *model, msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
 	if !m.query.initialized {
 		if len(m.args) == 0 {
 			m.args = append(m.args, "should ask for input")
@@ -36,16 +48,16 @@ func queryController(m *model, msg tea.Msg) (tea.Model, tea.Cmd) {
 		case tea.KeyMsg:
 			switch msg.Type {
 			case tea.KeyEnter:
-				m.config.openAIToken = m.config.tokenInput.Value()
-				m.config.tokenInput.Blur()
-
 				return m, nil
 
 			case tea.KeyEsc:
 				return m, tea.Quit
 			}
 		}
+
+		m.query.input.Focus()
+		m.query.input, cmd = m.query.input.Update(msg)
 	}
 
-	return m, nil
+	return m, cmd
 }
